070c0e0ef5db7145450ac77edbe9544946471714
hubIItech <hubbard2stephen@gmail.com>
2026-09-09 23:18:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-8 text-center">
