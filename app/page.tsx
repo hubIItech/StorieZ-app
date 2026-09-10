0e574a8b7eb12b89b263f4da7fab7951e5811a82
hubIItech <hubbard2stephen@gmail.com>
2026-09-09 23:38:55 -0400

    Remove unnecessary blank line in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-8 text-center">
